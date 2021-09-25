0878f76fba8c7eafaddbab4e595c7ccc7523e6b0
spiedra <jcspiedra@gmail.com>
2021-09-25 12:28:09 -0600

    uploading .NET project

---
+++ Gestor-Digital-ASADA-CL/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Gestor_Digital_ASADA_CL.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
