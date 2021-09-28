03d746a8d0c19851090d0ca3ce89c1392684272d
María F. Orocú Arias <mari.oro923@gmail.com>
2021-09-28 14:27:37 -0600

    Modulo avería completo

---
+++ Gestor-Digital-ASADA-CL/Controllers/Fault.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gestor_Digital_ASADA_CL.Controllers
+{
+    public class Fault : Controller
+    {
+        // GET: Fault
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Fault/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Fault/Create
+        public ActionResult InsertFault()
+        {
+            return View();
+        }
+
+        // POST: Fault/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult InsertFault(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Fault/Edit/5
+        public ActionResult EditFault(int id)
+        {
+            return View();
+        }
+
+        // POST: Fault/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult EditFault(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Fault/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Fault/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
