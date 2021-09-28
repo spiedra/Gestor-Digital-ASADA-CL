6907ca5f8b4717e429b78e92dd2e9607c8441daf
Randall <randms07@hotmail.com>
2021-09-27 18:59:41 -0600

    agregando tabla dkfe sql

---
+++ Gestor-Digital-ASADA-CL/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gestor_Digital_ASADA_CL.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: ProductController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        
+    }
+}
---
+++ Gestor-Digital-ASADA-CL/wwwroot/js/desahabilitarAyuda.js
@@ -0,0 +1,2 @@
+﻿
+$(".bienvenida").css("display", "none");
