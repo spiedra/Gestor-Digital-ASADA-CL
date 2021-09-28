bf1fc13eef094800f1621a5b2a33b918dc7725e4
Randall <randms07@hotmail.com>
2021-09-28 00:03:36 -0600

    bitacora personal terminada

---
+++ Gestor-Digital-ASADA-CL/wwwroot/js/PersonalBinnacle.js
@@ -0,0 +1,7 @@
+﻿function putOnModal(object) {
+    var activity = object.parentNode;
+    while (activity.id != "detalle") {
+        activity = activity.previousSibling;
+    }
+    $("#taDetalleModificar").val(activity.textContent);
+}
